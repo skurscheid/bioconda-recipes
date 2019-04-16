aa9bd6320e3a96a41254d310b7efa31360f684e6
Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
2019-04-16 00:32:11 +0000

    adds fix to missing zlib

--- recipes/genrich/build.sh
+++ recipes/genrich/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
 
 make
-mv Genrich $PREFIX/bin
+mv Genrich $PREFIX/bin && chmod +x $PREFIX/bin/Genrich

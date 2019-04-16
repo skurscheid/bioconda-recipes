46c2ea8a9634dface074a4fbfccaf9dec3b7b334
Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
2019-04-16 02:07:25 +0000

    amends build script

--- recipes/genrich/build.sh
+++ recipes/genrich/build.sh
@@ -2,11 +2,9 @@
 
 # https://bioconda.github.io/troubleshooting.html#zlib-errors
 export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
-mkdir -p $PREFIX/bin
-
 make
-mv Genrich $PREFIX/bin && chmod +x $PREFIX/bin/Genrich
+mkdir -p $PREFIX/bin
+cp Genrich $PREFIX/bin && chmod +x $PREFIX/bin/Genrich

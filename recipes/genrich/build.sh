cd2fd7e8d5092964fd27d16d175ca4604d4b2144
Sebastian Kurscheid <sebastian.kurscheid@anu.edu.au>
2019-04-16 01:42:28 +0000

    attempts to fix issue during build

--- recipes/genrich/build.sh
+++ recipes/genrich/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+# https://bioconda.github.io/troubleshooting.html#zlib-errors
 export CFLAGS="-I$PREFIX/include"
+export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
